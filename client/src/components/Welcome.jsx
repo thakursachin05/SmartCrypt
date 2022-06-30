aabf0d8281ca8bef6140558bc0ee5e601316dd26
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-07-01 00:07:11 +0530

    Update Welcome.jsx

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -23,7 +23,7 @@ const Input = ({ placeholder , name , type , value , handleChange }) => (
 );
 
 const Welcome = () => {
-  const { connectWallet, currentAccount , formData , handleChange , sendTransaction} = useContext(TransactionContext);
+  const { connectWallet, currentAccount , formData , handleChange , sendTransaction, isLoading} = useContext(TransactionContext);
   const handleSubmit = (e) => {
     const {addressTo , amount, keyword, message} = formData;
 
@@ -91,7 +91,7 @@ const Welcome = () => {
               <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange}/>
               <div className="h-[1px] w-full bg-gray-400 my-2" />
 
-              {false ?  (
+              {isLoading ?  (
                   <Loader />
               ) : ( <button type="button" onClick={handleSubmit} className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer">Send Now</button>
                 )}
