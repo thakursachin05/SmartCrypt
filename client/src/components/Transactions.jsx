63478d18258d0c4c67a089a8f6311e5c096a1a7a
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-06-30 15:46:55 +0530

    Update Transactions.jsx

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,7 +1,55 @@
-const Transactions = () => {
-    return  (
-        <h1>Transactions</h1>
+import React, { useContext } from 'react';
+import { TransactionContext } from '../context/TransactionContext';
+import dummyData from '../utils/dummyData';
+import { shortenAddress } from '../utils/shortenAddress';
 
+const TransactionCard = ({addressTo , addressFrom, timestamp, message, keyword, amount, url}) => {
+    return (
+        <div className='bg-[#181918] m-4 flex flex-1
+            2xl: min-w-[450px]
+            2xl: max-w-[500px]
+            sm: min-w-[270px]
+            sm: max-w-[300px]
+            flex-col p-3 rounded-md hover:shadow-2xl
+        '>
+            <div className='flex flex'>
+
+            </div>
+
+        </div>
+
+    )
+}
+
+const Transactions = () => {
+    const { currentAccount } =  useContext(TransactionContext);
+    return  (
+        <div className='flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions'>
+            <div className='flex flex-col md:p-12 py-12 px-4'>
+             {/* Latest Transactions  */}
+             {currentAccount ? 
+             ( 
+                <h3 className='text-white text-3xl text-center my-2'>
+                    Latest Transactions
+                </h3>
+             ) : 
+             (
+                <h3 className='text-white text-3xl text-center my-2'>
+                Connect your account to see the latest transactions
+            </h3>
+             )
+            }
+             {/* Connect your account to see the latest transactions */}
+
+            </div>
+
+            <div className='flex flex-wrap justify-center items-center mt-10'>
+                {dummyData.reverse().map((transaction, i) => (
+                    <TransactionCard key={i} {...transaction}/>
+                ))}
+            </div>
+
+        </div>
     );
 }
 
