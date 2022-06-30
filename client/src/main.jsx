ddc4fe45e37407900364525eea7d795b34cda000
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-06-30 13:00:44 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import { TrasacationProvider } from "./context/TransactionContext";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <TrasacationProvider>  
   <React.StrictMode>
     <App />
   </React.StrictMode>
+</TrasacationProvider>
 )
